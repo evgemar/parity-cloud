143986486a40028471783e7c0ea118f4b0378dc8
Alexander Krupenkin <mail@akru.me>
2017-01-26 19:14:27 +0300

    Password link

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # First init
-if [ ! -f password.txt ]; then
+if [ ! -f /root/.parity/keys/password.txt ]; then
   choose() { echo ${1:RANDOM%${#1}:1} $RANDOM; }
   echo "$({ choose '!@#$%^\&'
     choose '0123456789'
@@ -12,7 +12,10 @@ if [ ! -f password.txt ]; then
       choose '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     done
     } | sort -R | awk '{printf "%s",$1}')" > /root/.parity/keys/password.txt
-  parity account new --password /root/.parity/keys/password.txt 2>&1 | awk '{print $4}' > /root/.parity/keys/address.txt
+  ln -s /root/.parity/keys/password.txt
+  parity account new --password password.txt 2>&1 | awk '{print $4}' > /root/.parity/keys/address.txt
+else
+  ln -s /root/.parity/keys/password.txt
 fi
 
 ADDRESS="$(cat /root/.parity/keys/address.txt)"
@@ -26,5 +29,5 @@ exec parity --warp  \
             --jsonrpc-interface '0.0.0.0' \
             --jsonrpc-hosts='all' \
             --unlock 0x$ADDRESS    \
-            --password /root/.parity/keys/password.txt \
+            --password password.txt \
             --chain=$CHAIN $@
